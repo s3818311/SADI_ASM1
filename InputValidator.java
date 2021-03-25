6785bd78267cd22309760ab7ae7c9b82125f65b4
s3818311 <s3818311@rmit.edu.vn>
2021-03-25 14:26:17 +0700

    input validator basic implementation

---
+++ InputValidator.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class InputValidator {
+    private EnrolmentManager manager = EnrolmentManager.getInstance();
+    private Scanner scanner = Main.scanner;
+    private Pattern studentIdPattern = Pattern.compile("^\\d{7}$");
+    private Pattern courseIdPattern = Pattern.compile("^[A-Z]{4}\\d{4}$");
+    private Pattern semesterPattern = Pattern.compile("^\\d{4}[ABC]$");
+
+    private InputValidator() {
+    };
+
+    private static InputValidator INSTANCE = null;
+
+    public static InputValidator getInstance() {
+        if (INSTANCE == null)
+            INSTANCE = new InputValidator();
+
+        return INSTANCE;
+    }
+
+    public String getValidatedStudentId(String prompt) {
+        while (true) {
+            System.out.print(prompt);
+            String sid = scanner.next();
+            if (sid.length() == 8 && sid.startsWith("s") && studentIdPattern.matcher(sid.substring(1)).matches()) {
+                return sid;
+            } else {
+                System.out.println("Please enter the student ID in the correct format.");
+            }
+        }
+    }
+
+    public String getValidatedCourseId(String prompt) {
+        while (true) {
+            System.out.print(prompt);
+            String cid = scanner.next();
+            if (cid.length() == 8 && courseIdPattern.matcher(cid).matches()) {
+                return cid;
+            } else {
+                System.out.println("Please enter the course ID in the correct format.");
+            }
+        }
+    }
+
+    public String getValidatedSemester(String prompt) {
+        while (true) {
+            System.out.print(prompt);
+            String semester = scanner.next();
+            if (semester.length() == 5 && semesterPattern.matcher(semester).matches()) {
+                return semester;
+            } else {
+                System.out.println("Please enter the semester in the correct format.");
+            }
+        }
+    }
+
+}
